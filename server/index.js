91a0024975a59c2e79e898ce7971a7e440fbf91d
Zakchap2020 <zakarichapman@developersinstitute.co.nz>
2022-08-04 19:50:57 +1200

    get all and get id routes

--- server/index.js
+++ server/index.js
@@ -13,6 +13,12 @@ app.use(express.json());
 
 app.post('/books', async (req, res) => {
 	try {
+		const { description } = req.body;
+		const newBook = await pool.query(
+			'INSERT INTO books (description) VALUES($1) RETURNING *',
+			[description]
+		);
+		res.json(newBook.rows[0]);
 		console.log(req.body);
 	} catch (err) {
 		console.error(err.message);
@@ -21,12 +27,42 @@ app.post('/books', async (req, res) => {
 });
 
 //Get all books
-
+app.get('/books', async (req, res) => {
+	try {
+		const allBooks = await pool.query('SELECT * FROM books');
+		res.json(allBooks.rows);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
 //Get a book
+app.get('/books/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const allBooks = await pool.query(
+			'SELECT * FROM books WHERE book_id = $1',
+			[id]
+		);
+		console.log(allBooks.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
 
-//Post a book
-
-//Order a book
+//Update a book
+app.put('/books/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { description } = req.body;
+		const updateTodo = await pool.query(
+			'UPDATE todo SET description = $1 WHERE todo_id =$2',
+			[description, id]
+		);
+		console.log(allBooks.rows[0]);
+	} catch (err) {
+		console.error(err.message);
+	}
+});
 app.listen(5000, () => {
 	console.log('server has started on port 5000');
 });
